df0e1b19a2e5d5166987ddc1ee13919d5b00a48d
camelial <camelial@hawaii.edu>
2018-10-23 12:13:33 -1000

    Imported collections to server main

--- app/server/main.js
+++ app/server/main.js
@@ -1,4 +1,15 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/bathrooms/bathrooms.js';
+import '../imports/api/bathrooms/bathroomLocations.js';
+import '../imports/api/courses/courses.js';
+import '../imports/api/courses/courseLocations.js';
+import '../imports/api/events/events.js';
+import '../imports/api/events/eventLocations.js';
+import '../imports/api/locations/locations.js';
+import '../imports/api/users/users.js';
+import '../imports/api/users/userEvents.js';
+import '../imports/api/users/userCourses.js';
+
 
 Meteor.startup(() => {
   // code to run on server at startup
