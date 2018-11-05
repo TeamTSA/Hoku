f359aa064b37348ccea2c46704f140b4d2d960d5
WendyChen9 <31325518+WendyChen9@users.noreply.github.com>
2018-11-04 20:02:35 -1000

    render landing page

--- app/imports/ui/Layouts/App.jsx
+++ app/imports/ui/Layouts/App.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import landing from '../pages/landing-page.html';
 
 // App component - represents the whole app
 export default class App extends Component {
 
   render() {
     return (
-        <div className="container">
-          <header>
-            <h1>This is our app</h1>
-          </header>
-        </div>
+        <div src={landing }></div>
     );
   }
 }
